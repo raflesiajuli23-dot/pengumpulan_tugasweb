11f0624ba841d5619218577a851d11923c71c847
raflesiajuli23-dot <raflesiajuli23@gmail.com>
2026-07-07 23:27:52 +0700

    update ke 2

---
+++ src/Catalog.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from "react";
+
+class Catalog extends React.Component {
+    constuructor() {
+        super();
+        this.books = [
+            ["buku1.jpg", "Menguasai Pemograman Berorientasi Objek", "Ade Rahmat Iskandar", "Informatika", 2020],
+            ["buku2.jpg", "Dasar-Dasar Pemrograman dengan .NET", "Ade Rahmat Iskandar", "Informatika", 2019],
+            ["buku3.jpg", "Metodologi Pengembangan Sistem Informasi", "Ade Rahmat Iskandar", "Informatika", 2017],
+            ["buku4.png", "Struktur Data", "Rosa A.S", "Modula", 2018],
+            ["buku5.jpg", "Dasar Pemograman Python 3", "Abdul Kadir", "Andi Publisher", 2018],
+            ["buku6.jpeg", "Sistem Basis Data Dan Sql", "Didik Setiyadi", "Mitra Wacana Media", 2020],
+            ["buku7.jpg", "Perancangan Basis Data Teori", "Suhartini", "Deepublish", 2020],
+            ["buku8.jpg", "Teori Dan Praktek Sistem Operasi", "Zaid Romegar Mair", "Deepublish", 2018],
+        ];
+    }
+
+    render() {
+        let bookList = [];
+        for (let i = 0; i < this.books.length; i++) {
+            let book = this.books[i];
+            bookList.push(
+                <div className="col-sm-6 col-lg-4 mb-4 book">
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <img src={`img/${book[0]}`} width="150px" style={{marginBottom:"10px"}}/>
+                            <h5 className="card-title title">{book[1]}</h5>
+                            <p className="card-text author">{book[2]}</p>
+                            <p className="card-text text-muted publisher">{book[3]}
+                            <small className="year">{book[4]}</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return (
+            <Fragment>
+                {bookList}
+            </Fragment>
+        )
+    }
+}
